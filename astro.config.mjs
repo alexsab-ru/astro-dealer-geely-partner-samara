c693e035dc7e8c45a98510dcd7cea7b67afb8a45
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2026-02-19 12:00:06 +0500

    added &year&yprqee on cleanParam

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,7 +67,7 @@ export default defineConfig({
 						"utm_&utm_bn_id&utm_bn_system_id&utm_branch&utm_c&utm_campai&utm_campaign&utm_content&utm_from&utm_hclid&utm_me&utm_mediu&utm_medium&utm_orderpage&utm_partner_id&utm_placement&utm_position&utm_redirect&utm_referer&utm_referrer&utm_source&utm_startpage&utm_term&utm_type&_openstat&_source_stat_&gtm_debug", 
 						"adid&admitad_uid&adrclid&adv&aid&baobab_event_id&bxajaxid&calltouch_tm&clid&dclid&erid&etext&fbclid&from&frommarket&height&int_campaign&lang&length&mindbox&mindbox_nocache&mindbox-click-id&mindbox-message-key&mobileApp&msclkid&msisdn&mt_click_id&noredirect&openstat&order&ORDER_BY", 
 						"pay&payment&q&s&rb_clickid&ref&referrer&roistat_visit&sa&set_filter&source&tag&tags&target&text&token&twclid&type&types&userDataToken&USERID&uuid&ved&vendor&wbraid&width&action&register&cid&k50id&search&cm_id&ivid&hl&tpclid", 
-						"region&region_name&utm_ya_campaign&utm_candidate&block&model&color&drive&complects&editionuid"
+						"region&region_name&utm_ya_campaign&utm_candidate&block&model&color&drive&complects&editionuid&year&yprqee"
 					],
 				},
 				{
